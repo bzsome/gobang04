fd3e88ebee52bf0b79a7c58fd972ff5da59e0398
bzsome <15228569723asdf>
2018-11-22 15:10:47 +0800

    修改跨域设置(以前使用过滤器，现在使用拦截器)

--- server-oauth/src/main/java/com/chao/security/AuthorizationInterceptor.java
+++ server-oauth/src/main/java/com/chao/security/AuthorizationInterceptor.java
@@ -19,12 +19,11 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         logger.info("此页面需要验证身份：{}", request.getServletPath());
+
         //如果不是映射到方法直接通过
         if (!(handler instanceof HandlerMethod)) {
             return true;
         }
-        HandlerMethod handlerMethod = (HandlerMethod) handler;
-        Method method = handlerMethod.getMethod();
         //从header中得到token
         String authorization = request.getHeader(Constants.AUTHORIZATION);
         //验证token
@@ -32,6 +31,7 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
         if (username != null) {
             return true;
         }
+
         //如果验证token失败，并且方法注明了Authorization，返回401错误
         /*     if (method.getAnnotation(Authorization.class) != null) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
